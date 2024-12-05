b611aa2744b7b6dc53fafd4e6d22c2109aea50b0
FelipeAndresBascunanMorales <f.bascunan03@gmail.com>
2024-12-05 03:28:39 -0300

    feat: function that triggers a other function - because appwrite

---
+++ functions/function-trigger/src/main.js
@@ -0,0 +1,48 @@
+import { ExecutionMethod } from 'appwrite';
+import { Client, Users, Functions } from 'node-appwrite';
+
+// This Appwrite function will be executed every time your function is triggered
+export default async ({ req, res, log, error }) => {
+  // You can use the Appwrite SDK to interact with other services
+  // For this example, we're using the Users service
+  const client = new Client()
+    .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
+    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    .setKey(req.headers['x-appwrite-key'] ?? '');
+  const users = new Users(client);
+
+  try {
+    //call the other function
+    const functions = new Functions(client);
+    const result = await functions.createExecution(
+      process.env.POPULATE_INTEGRATIONS_FUNCION_ID,
+      "cron: */10 * * * 3",
+      false,
+      "",
+      ExecutionMethod.POST,
+      {},
+    );
+
+    log("Function executed successfully", result);
+    
+
+    // Log messages and errors to the Appwrite Console
+    // These logs won't be seen by your end users
+  } catch(err) {
+    error("Could not list users: " + err.message);
+  }
+
+  // The req object contains the request data
+  if (req.path === "/ping") {
+    // Use res object to respond with text(), json(), or binary()
+    // Don't forget to return a response!
+    return res.text("Pong");
+  }
+
+  return res.json({
+    motto: "Build like a team of hundreds_",
+    learn: "https://appwrite.io/docs",
+    connect: "https://appwrite.io/discord",
+    getInspired: "https://builtwith.appwrite.io",
+  });
+};
